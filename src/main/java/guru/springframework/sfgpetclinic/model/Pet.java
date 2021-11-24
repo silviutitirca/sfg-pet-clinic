e9931c0808e5127057df517dd40da6b865c234a8
silviutitirca <s_titirca@yahoo.com>
2021-11-24 14:48:17 +0100

    Adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,37 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * @author Silviu_Titirca on 11/24/2021
+ */
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDay;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(LocalDate birthDay) {
+        this.birthDay = birthDay;
+    }
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * @author Silviu_Titirca on 11/24/2021
+ */
+public class PetType {
+}
