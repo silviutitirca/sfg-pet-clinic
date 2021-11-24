831b35bbd2a635063613e54b2d6e616382b83965
silviutitirca <s_titirca@yahoo.com>
2021-11-24 16:39:57 +0100

    Update with correct filename

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework.sfgclinic;
+package guru.springframework.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
