1e551d76d6748bfd40cb613d8dff4d5ec335e122
silviutitirca <s_titirca@yahoo.com>
2021-11-30 11:02:32 +0100

    Add PetTypeMapService. Closes #29.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * @author Silviu_Titirca on 11/30/2021
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+}
