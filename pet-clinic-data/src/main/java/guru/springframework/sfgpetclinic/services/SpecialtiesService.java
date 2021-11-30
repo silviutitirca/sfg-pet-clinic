8a8409f239f473b95dc785df3724794b86f4f01d
silviutitirca <s_titirca@yahoo.com>
2021-11-30 12:31:01 +0100

    Create Specialty Map Service Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+
+/**
+ * @author Silviu_Titirca on 11/30/2021
+ */
+public interface SpecialtiesService extends CrudService<Speciality, Long>{
+
+}
