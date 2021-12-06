5625e59a478d2646859321ae05c043fe2a281592
silviutitirca <s_titirca@yahoo.com>
2021-12-06 16:57:39 +0100

    Added VisitMapService. Closes #46.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+
+/**
+ * @author Silviu_Titirca on 12/6/2021
+ */
+public interface VisitService extends CrudService<Visit, Long>{
+
+
+}
