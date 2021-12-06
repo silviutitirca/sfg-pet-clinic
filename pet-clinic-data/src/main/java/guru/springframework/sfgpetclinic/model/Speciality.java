935fc379e4f4e31c3de7d5c03d2ea5c3b03f9287
silviutitirca <s_titirca@yahoo.com>
2021-12-06 12:12:54 +0100

    Updated Vets with JPA Entity. Closes #37.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,9 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author Silviu_Titirca on 11/30/2021
  */
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity{
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
