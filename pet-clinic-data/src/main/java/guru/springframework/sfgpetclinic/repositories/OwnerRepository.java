033e5b1881d7c693881c3eedcf863ad10e41524a
silviutitirca <s_titirca@yahoo.com>
2021-12-06 14:49:13 +0100

    Added Spring Data JPA Service for Onwers. Closes #40.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.repository.CrudRepository;
  * @author Silviu_Titirca on 12/6/2021
  */
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
 }
