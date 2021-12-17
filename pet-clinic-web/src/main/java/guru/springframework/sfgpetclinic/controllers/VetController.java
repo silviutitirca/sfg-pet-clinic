4dae9ba7cb4b9fcb00920a25f98790c0ad72a680
silviutitirca <s_titirca@yahoo.com>
2021-12-17 12:43:27 +0100

    Implemented Vets List View. Closes #63.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * @author Silviu_Titirca on 11/26/2021
@@ -17,10 +22,17 @@ public class VetController {
         this.vetService = vetService;
     }
 
-
-    @RequestMapping({"/vets", "/vets/index","/vets/index.html", "/vets.html"})
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model){
+
         model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+
+        return vetService.findAll();
     }
 }
